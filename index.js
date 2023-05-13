8f78cbfbe47334425ec8d9320852692a7860ae2a
md al-amin <muhammadalamin809@gmail.com>
2023-05-14 02:17:23 +0800

    bookings data added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,47 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const servicesCollection = client.db('carDoctor').collection('services');
+        const checkoutCollection = client.db('carDoctor').collection('checkout');
+
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                // Include only the `title` and fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
+            const result = await servicesCollection.findOne(query, options)
+            res.send(result);
+
+        });
+
+        // checkout
+
+        app.get('/checkout', async(req, res) => {
+            let query = {};
+            if(req.query?.email){
+                query = {email: req.query.email}
+            }
+            const result = await checkoutCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/checkout', async(req, res) => {
+            const checkout = req.body;
+            const result = await checkoutCollection.insertOne(checkout);
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
